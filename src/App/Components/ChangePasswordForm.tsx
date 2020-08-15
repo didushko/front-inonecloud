1c9a7860df3e6c30e81c3afe3e21be4d06474e0c
Andrew <andrewyelmanov@gmail.com>
2020-08-15 16:03:35 +0300

    Исправлена форма смены пароля

--- src/App/Components/ChangePasswordForm.tsx
+++ src/App/Components/ChangePasswordForm.tsx
@@ -7,10 +7,10 @@ import axios, {AxiosResponse} from "axios";
 interface IState {
     currentPassword: string,
     newPassword: string,
-    rPassword:string,
+    rPassword: string,
     validation: boolean,
-    error:string,
-    changePassword:string
+    error: string,
+    changePassword: string
 }
 
 function ChangePasswordForm(): JSX.Element {
@@ -18,7 +18,7 @@ function ChangePasswordForm(): JSX.Element {
     const language: Language = getLanguage(store.getState().language);
 
 
-    let [formState, setFormState] = useReducer((state: IState, newState: object) =>{
+    let [formState, setFormState] = useReducer((state: IState, newState: object) => {
         let st = {...state, ...newState};
         st = {...st, ...validation(st)};
         return st;
@@ -31,35 +31,45 @@ function ChangePasswordForm(): JSX.Element {
         changePassword: ''
     });
 
-    function validation(st: IState){
-
+    function validation(st: IState) {
+        if(st.newPassword != st.rPassword){
+            st.error="Passwords are not equals" //todo change to variable
+            st.validation=false;
+        }
+        if(st.currentPassword == st.newPassword){
+            st.error = "New password must not be equal current password"; //todo change to variable
+            st.validation = false;
+        }
+        else {
+            st.error='';
+            st.validation = true;
+        }
         return st;
     }
 
 
-
     function changeInput(e: any) {
-        setFormState({[e.target.name] : e.target.value});
+        setFormState({[e.target.name]: e.target.value});
         console.log(formState);
-
     }
 
     async function changePassword() {
         try {
             let result: AxiosResponse = await axios.post(
                 "http://localhost:8080/api/user/changePassword",
+                //todo add authorization header
                 {
                     current_password: formState.currentPassword,
                     new_password: formState.newPassword
 
                 },
             );
-            if(result.status===201){
-                setFormState({changePassword:language.Settings.changePassword});
+            if (result.status === 201) {
+                setFormState({changePassword: language.Settings.changePassword});
             }
             //window.location.href = "/";
         } catch (e) {
-            switch(e.response.status){
+            switch (e.response.status) {
                 // case 409: setFormState({registration:language.SignUp.form.errors.userExist}); break;
                 // case 400: setFormState({registration: language.SignUp.form.errors.incorrectly}); break;
                 // default: setFormState({registration:language.SignUp.form.errors.server}); break;
@@ -69,16 +79,18 @@ function ChangePasswordForm(): JSX.Element {
     }
 
     return (
-        <form id='change_password' method='post' action="?">
+        <form id='registration' method='post' action="?">
             <fieldset id="inputs">
-                <input name='oldPassword' type='password' placeholder={language.Settings.changePasswordForm.oldpassword} required={true}
+                <input name='currentPassword' type='password'
+                       placeholder={language.Settings.changePasswordForm.oldpassword} required={true}
                        pattern='[0-9a-zA-Z]{6,32}' title='Password should have 6 sybols with digits'
                        className='password'
                        value={formState.currentPassword}
                        onChange={changeInput}
                 />
 
-                <input name='newPassword' type='password' placeholder={language.Settings.changePasswordForm.newpassword} required={true}
+                <input name='newPassword' type='password' placeholder={language.Settings.changePasswordForm.newpassword}
+                       required={true}
                        pattern='[0-9a-zA-Z]{6,32}' title='Password should have 6 sybols with digits'
                        className='password'
                        value={formState.newPassword}
